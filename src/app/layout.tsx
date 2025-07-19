b424ecd9d70828c0239af7e10790d8104e789f68
Qasim Rokeeb <qasimrokeeb@gmail.com>
2025-07-19 06:46:38 +0100

    configure website for web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
@@ -47,6 +48,7 @@ export default function RootLayout({
       <body className="font-body antialiased bg-background">
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
